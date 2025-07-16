1590d02d57ce32048245ded44d0a86b4e51988a4
Stepainpy <stepa2014vlg@gmail.com>
2025-07-16 12:26:39 +0300

    remove pointer check

--- src/brainfuck.c
+++ src/brainfuck.c
@@ -420,8 +420,6 @@ cleanup:
 }
 
 void bfd_instrs_dump_txt(bft_program* prog, FILE* dest, size_t limit) {
-    if (!prog) return;
-
     const int addr_width = floor(log10(prog->count - 2)) + 1;
     bft_instr* instr = prog->items;
     for (size_t i = 0; i < limit && *instr != BFI_DEAD; i++, instr++) {

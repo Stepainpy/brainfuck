6ae20f10c18efc5fe2ffc6af205b6503af2931ac
Stepainpy <stepa2014vlg@gmail.com>
2025-07-18 20:31:33 +0300

    add c++ guard

--- inc/brainfuck.h
+++ inc/brainfuck.h
@@ -4,6 +4,10 @@
 #include <stdio.h>
 #include "bfconf.h"
 
+#ifdef __cplusplus
+extern "C" {
+#endif
+
 const char* bfa_strerror(bft_error error);
 
 bft_error bfa_compile(bft_program* program, const char* code, size_t size);
@@ -15,6 +19,10 @@ void bfd_instrs_dump_txt(bft_program* program, FILE* dest, size_t limit);
 void bfd_memory_dump_txt(bft_context* context, FILE* dest, size_t offset, size_t size);
 void bfd_memory_dump_bin(bft_context* context, FILE* dest, size_t offset, size_t size);
 void bfd_memory_dump_loc(bft_context* context, FILE* dest);
+
+#ifdef __cplusplus
+}
+#endif
 
 #endif // BRAINFUCK_H
 
